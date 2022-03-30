c1468fddcfd9ac619118f84e0a33cc14dd8bdf88
Morgan <hx801@nyu.edu>
2022-03-29 21:40:46 -0400

    Init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Terminus
+This is a sentence boundary labeler by Haoran Xu, Zachary Waxman, and Sreeya Narra. 
---
+++ code/Terminus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Terminus
+//
+//  Created by Morgan on 3/29/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
